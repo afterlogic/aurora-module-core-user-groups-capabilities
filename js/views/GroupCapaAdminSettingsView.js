8539733023a507be80ffa0e192caf610e15367e5
nadine-shi <nadine@afterlogic.com>
2019-03-20 18:36:55 +0300

    Cache doesn't exist in the module

--- js/views/GroupCapaAdminSettingsView.js
+++ js/views/GroupCapaAdminSettingsView.js
@@ -14,7 +14,6 @@ var
 	ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js'),
 	CAbstractSettingsFormView = ModulesManager.run('AdminPanelWebclient', 'getAbstractSettingsFormViewClass'),
 	
-	Cache = require('modules/%ModuleName%/js/Cache.js'),
 	Settings = require('modules/%ModuleName%/js/Settings.js')
 ;
 
